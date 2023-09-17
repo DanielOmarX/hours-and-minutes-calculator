33e2a87724b28394c941c86b1e686e8707e14889
Daniel Omar <135461737+DanielOmarX@users.noreply.github.com>
2023-09-17 21:06:53 +0300

    hours and minutes calculator.py

--- hours and minutes calculator.py
+++ hours and minutes calculator.py
@@ -4,6 +4,4 @@ second = int(input("Enter Your duration in seconds: \n"))
 Hours = second // 3600
 Minutes = (second % 3600) // 60
 remaing_second = second % 60
-print(
-    f"The duratoin is :{Hours} Hours, {Minutes} Minutes, and {remaing_second} seconds"
-)
+print( f"The duratoin is :{Hours} Hours, {Minutes} Minutes, and {remaing_second} seconds")
